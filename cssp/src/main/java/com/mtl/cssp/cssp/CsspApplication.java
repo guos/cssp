36f4b5cce3a51881f60422cb6f67d98bb2d38d5c
Administrator <Administrator@gggg>
2020-01-18 21:47:38 +0800

    init

---
+++ cssp/src/main/java/com/mtl/cssp/cssp/CsspApplication.java
@@ -0,0 +1,13 @@
+package com.mtl.cssp.cssp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsspApplication.class, args);
+	}
+
+}
---
+++ cssp/src/test/java/com/mtl/cssp/cssp/CsspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtl.cssp.cssp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
